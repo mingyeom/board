0f68dfc2a7a508b2aa5a2b506922465ca05f2b6d
mingyeom <zau96@naver.com>
2018-07-27 12:13:19 +0900

    복붙의 참사

--- src/main/java/kr/or/ddit/upload/service/UploadService.java
+++ src/main/java/kr/or/ddit/upload/service/UploadService.java
@@ -2,12 +2,6 @@ package kr.or.ddit.upload.service;
 
 import java.util.List;
 
-import org.junit.Before;
-
-import kr.or.ddit.board.model.BoardVo;
-import kr.or.ddit.post.dao.PostDao;
-import kr.or.ddit.post.dao.PostDaoInf;
-import kr.or.ddit.post.model.PostVo;
 import kr.or.ddit.upload.dao.UploadDao;
 import kr.or.ddit.upload.dao.UploadDaoInf;
 import kr.or.ddit.upload.model.UploadVo;
